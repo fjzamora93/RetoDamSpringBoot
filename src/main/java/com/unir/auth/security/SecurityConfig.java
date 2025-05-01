ed69f4133a2e82334cdd5e4a7946282e8077b168
fjzamora93 <fjzamora93@gmail.com>
2025-05-01 16:04:10 +0200

    allow request from all origin

--- src/main/java/com/unir/auth/security/SecurityConfig.java
+++ src/main/java/com/unir/auth/security/SecurityConfig.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -46,10 +47,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para APIs REST
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // Permitir acceso público a /api/auth/**
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/api/empresas/**",
+                                "/api/categorias/**",
+                                "/api/vacantes/**",
+                                "/api/solicitudes/**"
+                                ).permitAll() // Permitir acceso público a /api/auth/**
                         .anyRequest().authenticated() // El resto de las rutas requieren autenticación
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
